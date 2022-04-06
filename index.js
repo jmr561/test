39b4048fa6caed37488b614b9c2c2c883b9c9e92
Josh <joshua.m.rivas@gmail.com>
2022-04-06 18:28:43 +0200

    test completed

---
+++ index.js
@@ -0,0 +1,23 @@
+const closestToZero = (arr) => {
+  if (!arr || arr.length === 0) return 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!Number.isInteger(arr[i]))
+      return "All elements of array must be integers";
+  }
+
+  let absoluteArr = arr.map((x) => Math.abs(x));
+  let closestIntAbsVal = Math.min(...absoluteArr);
+
+  return arr.indexOf(closestIntAbsVal) !== -1
+    ? arr[arr.indexOf(closestIntAbsVal)]
+    : arr[arr.indexOf(-1 * closestIntAbsVal)];
+};
+
+console.log(closestToZero([]));
+console.log(closestToZero(undefined));
+console.log(closestToZero());
+console.log(closestToZero([-1.5, 0, 1, 2, 3]));
+console.log(closestToZero([-1, 1, 2, 3]));
+console.log(closestToZero([-1, 2, 3, 4]));
+console.log(closestToZero([100, 101, -200, 30]));
